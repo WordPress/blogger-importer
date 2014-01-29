b4d0cff3b36ed4ea3c4d3377c56d917d5c474d34
Workshopshed <workshopshed@git.wordpress.org>
2014-01-29 14:17:09 +0000

    Fix to license header
    git-svn-id: https://plugins.svn.wordpress.org/blogger-importer/trunk@847682 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- blogger-importer.php
+++ blogger-importer.php
@@ -7,7 +7,8 @@ Description: Imports posts, comments, images and tags from a Blogger blog then m
 Author: wordpressdotorg
 Author URI: http://wordpress.org/
 Version: 0.7
-License: GPL v2 - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+License: GPL v2
+License URI: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 */
 
 // Wordpress Classes needed by importer
