90cd5ef4fa38a651ef7fe4cc2cac8c0da55623b1
Workshopshed <workshopshed@git.wordpress.org>
2014-03-03 18:10:01 +0000

    Simplified Exists function to improve performance and stop comments being marked as a duplicate when they are not.
    git-svn-id: https://plugins.svn.wordpress.org/blogger-importer/trunk@868719 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- comment-entry.php
+++ comment-entry.php
@@ -69,20 +69,7 @@ if (!class_exists('CommentEntry'))
         {
             //Do we have 2 comments for the same author at the same time, on the same post?
             //returns comment id
-            
-            //Updated to first check the internal id
-            //Can we use get_comments here?
-            global $wpdb;
-            $c = $this->get_comment_by_oldID($this->self);
-            if ($c <> 0) {
-                return ($c);
-                }
-            else {
-                $commentID = $wpdb->get_var($wpdb->prepare("SELECT comment_ID FROM $wpdb->comments
-			                                             WHERE comment_post_ID = %s and comment_author = %s AND comment_date = %s",
-                                                $this->post_ID, $this->author, $this->updated));
-                return ($commentID);
-            }
+            return ($this->get_comment_by_oldID($this->self));
         }
         
         function get_comment_by_oldID($oldID) {
@@ -91,6 +78,7 @@ if (!class_exists('CommentEntry'))
             global $wpdb;
             $query = "SELECT c.comment_id FROM $wpdb->commentmeta m inner join $wpdb->comments c on c.comment_ID = m.comment_id where meta_key = 'blogger_internal' and meta_value = '%s' LIMIT 0 , 1";
             $c = (int) $wpdb->get_var( $wpdb->prepare($query, $oldID) );
+            
             return $c;            
         }
 
